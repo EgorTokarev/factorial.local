0e6add37a50005b31148670c366329a515705d39
EgorTokarev <zharu.tokarev@gmail.com>
2021-03-17 10:26:59 +0300

    1.4 Создание метода для загрузки формы

--- app/Http/Controllers/ConverterController.php
+++ app/Http/Controllers/ConverterController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ConverterController extends Controller
 {
-    //
+    /**
+     * Точка входа в приложение
+     *
+     * @return View представление
+     */
+    public function index()
+    {
+        return view('index');
+    }
 }
