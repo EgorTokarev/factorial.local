393dc0a2d5aca5b4813464ca6cb51bb32d545af2
EgorTokarev <zharu.tokarev@gmail.com>
2021-03-17 10:48:48 +0300

    1.5 Создание базового предстовления о приложении

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Factorial</title>
+</head>
+<body>
+
+<!-- Результат -->
+
+<!-- Форма для передачи данных -->
+
+</body>
+</html>
