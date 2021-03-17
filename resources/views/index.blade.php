45db860d6f935d68c2d59f4d219903b3ed781261
EgorTokarev <zharu.tokarev@gmail.com>
2021-03-17 11:24:50 +0300

    1.5.2 Форма для передачи данных

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
+    <head>
+        <meta charset="UTF-8">
 
-    <title>Factorial</title>
-</head>
-<body>
+        <title>Factorial</title>
+    </head>
+    <body>
 
-<!-- Результат -->
-<p>
-    <b>Результат</b>: {{ $factorial ?? '' }}
-</p>
-<!-- Форма для передачи данных -->
-
-</body>
+        <!-- Результат -->
+        <p>
+            <b>Результат</b>: {{ $factorial ?? '' }}
+        </p>
+        <!-- Форма для передачи данных -->
+        <form action="{{ route('factorial') }}" method="post">
+            @csrf
+            <label for="number">Factorial</label>
+            <input type="text" name="number" id="number"
+            value="{{ $number ?? old('number') }}">
+            <button type="submit">Вычислить</button>
+        </form>
+        <!-- Ошибки -->
+    </body>
 </html>
