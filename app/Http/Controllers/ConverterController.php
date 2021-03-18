0a84ed45b452e58c4ad21d16c5f71f9089d9a7cc
EgorTokarev <zharu.tokarev@gmail.com>
2021-03-18 09:52:25 +0300

    2.2 Подключение валидации к контроллеру

--- app/Http/Controllers/ConverterController.php
+++ app/Http/Controllers/ConverterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\FactorialRequest;
 
 class ConverterController extends Controller
 {
@@ -18,18 +18,15 @@ class ConverterController extends Controller
     /**
      * числение факториала
      *
-     * @param  request $request
+     * @param  FactorialRequest $request
      *
      * @return View представление
      */
-    public function factorial(Request $request)
+    public function factorial(FactorialRequest $request)
     {
         // Получение данных
         $number = $request->number;
-        // Валидация
-        $validated = $request->validate([
-            'number' => 'required|numeric|integer|min:0',
-        ]);
+
         // Вычисление факториала
         $factorial = 1;
         for ($i=1; $i <= $number; $i++) {
